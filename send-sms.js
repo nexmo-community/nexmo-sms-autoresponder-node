7edd4823b53805586dfe9de3d11e8365c7a0c822
Alex Lakatos <alex.lakatos.qa@gmail.com>
2019-09-09 23:13:22 +0800

    send sms example

---
+++ send-sms.js
@@ -0,0 +1,24 @@
+require('dotenv').config({path:__dirname + '/.env'});
+
+const Nexmo = require('nexmo');
+
+const nexmo = new Nexmo({
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET
+});
+
+var text = "👋Hello from Nexmo";
+
+nexmo.message.sendSms("Nexmo", process.env.TO_NUMBER, text, {
+  type: "unicode"
+}, (err, responseData) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if (responseData.messages[0]['status'] === "0") {
+      console.log("Message sent successfully.");
+    } else {
+      console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+    }
+  }
+})
