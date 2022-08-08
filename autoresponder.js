db97d751ee81a6c8e6a63c3f2e99ee84ff198bb7
Amanda Cavallaro <amanda.cavallaro@vonage.com>
2022-08-09 00:03:17 +0100

    Adds messages-api changes

--- autoresponder.js
+++ autoresponder.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import request from 'request'
 import Vonage from '@vonage/server-sdk'
+import SMS from '@vonage/server-sdk/lib/Messages/SMS.js'
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ const {
 const app = express()
 
 const vonage = new Vonage({
+  apiKey: process.env.VONAGE_API_KEY,
+  apiSecret: process.env.VONAGE_API_SECRET,
   applicationId: process.env.VONAGE_APPLICATION_ID,
   privateKey: process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH
 })
@@ -30,39 +33,27 @@ app.post('/webhooks/inbound', (req, res) => {
   console.log(req.body)
 
   let text = "The Numbers API has thrown an error."
-  const number = parseInt(req.body.message.content.text) || 42
+  const number = parseInt(req.body.text) || 42
 
   request(`http://numbersapi.com/${number}`, (error, response, body) => {
     if (!error) {
       text = body
     }
 
-    vonage.channel.send({
-        "type": "sms",
-        "number": req.body.from.number
-      }, {
-        "type": "sms",
-        "number": req.body.to.number
-      }, {
-        "content": {
-          "type": "text",
-          "text": text
-        }
-      },
-      (err, responseData) => {
+    vonage.messages.send(
+      new SMS(text, req.body.to, req.body.from),
+      (err, data) => {
         if (err) {
-          console.log("Message failed with error:", err)
+          console.error("Message failed with error:", err);
         } else {
-          console.log(`Message ${responseData.message_uuid} sent successfully.`)
+          console.log(`Message ${data.message_uuid} sent successfully.`);
         }
-      }
-    )
+      }, res.status(200).end()
+    );
 
-    res.status(200).end()
-  })
-
-  app.post('/webhooks/status', (req, res) => {
-    console.log(req.body)
-    res.status(200).end()
+    app.post('/webhooks/status', (req, res) => {
+      console.log(req.body)
+      res.status(200).end()
+    })
   })
 })
--- send-messages.js
+++ send-messages.js
@@ -1,29 +1,23 @@
 import dotenv from 'dotenv'
 import Vonage from '@vonage/server-sdk'
+import SMS from '@vonage/server-sdk/lib/Messages/SMS.js'
 
 dotenv.config()
 
 const vonage = new Vonage({
-  applicationId: process.env.VONAGE_APPLICATION_ID,
-  privateKey: process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH
+  apiKey: process.env.VONAGE_API_KEY,
+  apiSecret: process.env.VONAGE_API_SECRET
 });
 
 const text = "👋Hello from Vonage";
 
-vonage.channel.send(
-  { "type": "sms", "number": process.env.TO_NUMBER },
-  { "type": "sms", "number": "Vonage" },
-  {
-    "content": {
-      "type": "text",
-      "text": text
-    }
-  },
-  (err, responseData) => {
+vonage.messages.send(
+  new SMS(text, process.env.TO_NUMBER, "Vonage"),
+  (err, data) => {
     if (err) {
-      console.log("Message failed with error:", err);
+      console.error("Message failed with error:", err);
     } else {
-      console.log(`Message ${responseData.message_uuid} sent successfully.`);
+      console.log(`Message ${data.message_uuid} sent successfully.`);
     }
   }
 );
